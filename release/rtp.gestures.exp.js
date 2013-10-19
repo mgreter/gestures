45fe26942a89f640ac44596fc12e3cdcd0c62002
Marcel Greter <marcel.greter@ocbnet.ch>
2013-10-19 06:23:28 +0200

    Commits merged release files

--- release/rtp.gestures.exp.js
+++ release/rtp.gestures.exp.js
@@ -6,14 +6,6 @@
   either version 3 of the License, or (at your option) any later version.
 
 */;
-/*
-
-  Copyright (c) Marcel Greter 2011/2013 - OCBNET Gestures 0.0.0
-  This is free software; you can redistribute it and/or modify it under the terms
-  of the [GNU General Public License](http://www.gnu.org/licenses/gpl-3.0.txt),
-  either version 3 of the License, or (at your option) any later version.
-
-*/;
 // @@@ private scope @@@
 (function()
 {
--- release/rtp.gestures.full.js
+++ release/rtp.gestures.full.js
@@ -8,14 +8,6 @@
 */;
 /*
 
-  Copyright (c) Marcel Greter 2011/2013 - OCBNET Gestures 0.0.0
-  This is free software; you can redistribute it and/or modify it under the terms
-  of the [GNU General Public License](http://www.gnu.org/licenses/gpl-3.0.txt),
-  either version 3 of the License, or (at your option) any later version.
-
-*/;
-/*
-
   Copyright (c) Marcel Greter 2012 - OCBNET gesture 0.0.0
   This plugin available for use in all personal or commercial projects under both MIT and GPL licenses.
 
